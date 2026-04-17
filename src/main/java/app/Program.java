8cc0866805ef4274dbb1594a26746b609b0b5e68
iararoman <iara.roman@alu.frt.utn.edu.ar>
2026-04-17 10:30:19 -0300

    Se agrego Menu Principal

--- src/main/java/app/Program.java
+++ src/main/java/app/Program.java
@@ -2,15 +2,13 @@ package app;
 
 import data.Persistencia;
 import java.util.InvalidPropertiesFormatException;
-import views.ListarVehiculosView;
-import views.AgregarVehiculo;
+import views.MenuPrincipal;
+
 
 public class Program {
     public static void main(String[] args) throws IllegalArgumentException, InvalidPropertiesFormatException {
         Persistencia.inicializar();
-        ListarVehiculosView view = new ListarVehiculosView();
-        view.setVisible(true);
-        AgregarVehiculo v = new AgregarVehiculo();
-        v.setVisible(true);
+        MenuPrincipal v1 = new MenuPrincipal();
+        v1.setVisible(true);
     }
 }
